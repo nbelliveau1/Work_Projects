9736b1071613556a762878aa2ec570785fcc7910
nbelliveau1 <155105351+nbelliveau1@users.noreply.github.com>
2025-11-14 14:03:06 -0500

    Algorithms

---
+++ mergesort.c
@@ -0,0 +1,178 @@
+// Task: Implement the 'sortIntegers' function below
+
+// =================== Libraries ==================
+#include <stdio.h> // Include file for standard input/output
+#include <stdlib.h>
+#include <time.h>
+
+#define EXPERIMENT 0// use this for the experimentation in this lab
+
+// =============== Helper Functions ===============
+
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int temp[], int leftIndex, int m, int rightIndex) {
+  //1) Copy window to temp
+  for (int i = leftIndex; i <=rightIndex; i++){
+    temp[i] = arr[i];
+  }
+  
+  //Three iteratiors 
+  /*
+  i = iterator L-half
+  j = iterator R-half
+  k = iterator ordered merging array
+  */ 
+  int i = leftIndex;
+  int j = m+1;
+  int k =leftIndex;
+
+  while (i<=m && j<= rightIndex){
+    if(temp[i]<temp[j]){
+      arr[k++] = temp[i++]; //Post increment == Use current value for assignment, then increment
+    }else arr[k++] = temp[j++]; //Post increment == Use current value for assignment, then increment value
+  }
+  while (i<=m){arr[k++] = temp[i++];} //For odd subarrays left side 
+  while (j<=rightIndex){arr[k++] = temp[j++];} //For odd subarrays on right side 
+
+}
+ 
+
+void mergeSort(int arr[], int leftIndex, int rightIndex){
+    if (leftIndex >=rightIndex){
+    return; //Ends because we have reached subarrays containing just one element and no longer need to divide
+  }
+   //Math to determine halfway point 
+   /*
+   rightIndex-LeftIndex gives you distance between two indicies
+   Dividing by 2 halves that distance 
+   Adding leftIndex shifts the midpoint back to the correct positoi within the array
+   Prevents
+   */
+   int midIndex = leftIndex + (rightIndex - leftIndex)/2; //Midpoint of array
+
+    mergeSort(arr, leftIndex, midIndex);               // sort left half
+    mergeSort(arr, midIndex + 1, rightIndex);          // sort right half
+
+  
+    // Dynamically allocates temporary buffer for temp[], large enough to hold
+    // elements up to rightIndex (inclusive). This buffer is used by merge()
+    // to safely combine the two sorted subarrays.
+    int *temp = (int*)malloc((size_t)(rightIndex+1) * sizeof *temp);
+    if (!temp) return;                                  // handle alloc failure
+
+    merge(arr, temp, leftIndex, midIndex, rightIndex); // merge halves
+    free(temp);
+
+}
+ 
+
+
+
+
+// Provided below is a sort function. We have also
+// provided a template for how to document functions
+// to help organize your code.
+// Name: sortIntegers
+// Input(s):
+//          (1) 'array' is a pointer to an integer address. 
+//              This is the start of some 'contiguous block of memory' that we will sort.
+//          (2) 'size' tells us how big the array of data is we are sorting.
+// Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
+void sortIntegers(int* array, unsigned int size){
+    if(size<= 1) return;
+
+    mergeSort(array, 0, size-1);
+
+
+}
+
+
+// Input: A pointer to an array (i.e. the array itself points to the first index)
+//        The size of the array (Because we do not know how big the array is automatically)
+void printIntArray(int* array, unsigned int size){
+  unsigned int i; // Note: 'unsigned int' is a datatype for storing positive integers.
+  for(i = 0; i < size; i=i+1){
+    printf("%d ",array[i]);
+  }
+  printf("\n");
+}
+
+int main(int argc, char **argv){
+#if EXPERIMENT==0
+  // Some test data sets.
+  int dataset1[] = {0,1,2,3,4,5,6,7,8,9,10};
+  int dataset2[] = {10,9,8,7,6,5,4,3,2,1,0};
+  int dataset3[] = {0,3,2,1,4,7,6,5,8,9,10};
+  int dataset4[] = {2,1,1,1,1,1,1,1,1,1,1};
+  int dataset5[] = {100,201,52,3223,24,55,623,75,8523,-9,150};
+  int dataset6[] = {-1,1,2,-3,4,5,-6,7,8,-9,10};
+  
+  // Sort our integer array
+  sortIntegers(dataset1, 11);
+  sortIntegers(dataset2, 11);
+  sortIntegers(dataset3, 11);
+  sortIntegers(dataset4, 11);
+  sortIntegers(dataset5, 11);
+  sortIntegers(dataset6, 11);
+  
+  // Print out an array
+  printIntArray(dataset1, 11);
+  printIntArray(dataset2, 11);
+  printIntArray(dataset3, 11);
+  printIntArray(dataset4, 11);
+  printIntArray(dataset5, 11);
+  printIntArray(dataset6, 11);
+
+#else
+
+  int i=0;
+
+  if (argc != 2) {
+    printf("One argument expected: ./mergesort 1000\n");
+    return 1;
+  }
+
+  // Convert the argument of the program into an integer
+  const int size = atoi(argv[1]);
+  // Generate a random seed
+  time_t t;
+  srand((unsigned)time(&t));
+  // Allocate memory
+  int* random = (int*)malloc(sizeof(int)*size);
+
+  // Populate our test data set
+  for(i = 0 ; i < size ; i++) {
+    // Generate random values from 0 to 99
+    random[i] = rand()%size; 
+  }
+
+  // You can uncomment if you'd like to see the size sorted
+  // printf("Before the sort: ");
+  // printIntArray(random, size);
+
+  // Setup timers  
+  struct timespec begin, end;
+  // Get the time before we start
+  clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+  // Perform the sort
+  sortIntegers(random,size);
+  // Get the time after we are done
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+  double time_taken = (end.tv_nsec - begin.tv_nsec) / 1000000000.0 +
+            (end.tv_sec  - begin.tv_sec);
+  printf ("Total time = %f seconds\n", time_taken);
+
+  // Confirm the sort worked
+  // printf("After the sort: ");
+  // printIntArray(random, size);
+  
+  // Free our random array
+  free(random);
+
+#endif
+
+  return 0;
+}
